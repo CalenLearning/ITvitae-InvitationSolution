e1e661fc4304e8596fbe0d17610364b8bfad2ecf
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2021-05-09 22:16:57 +0200

    Testing if it does save sql now

---
+++ Database1/People.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Person] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [SurnamePrefix] VARCHAR(50) NULL, 
+    [Surname] VARCHAR(50) NOT NULL, 
+    [Gender] CHAR(10) NOT NULL, 
+    [FirstContactMethod] CHAR(10) NOT NULL, 
+    [SecondContactMethod] CHAR(10) NULL, 
+    [ThirdContactMethod] CHAR(10) NULL, 
+    [FourthContactMethod] CHAR(10) NULL
+)
